10c1813a0366701f4f942bd91d0e3952ba539ae6
Juan Dorado <fdorado985@gmail.com>
2020-10-30 15:11:01 -0600

    Add an array of questions

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,11 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
